f5397cadb2ed5a2b4954c941459c3d664052fdc5
tdwth-joe <153840684+tdwth-joe@users.noreply.github.com>
2023-12-16 22:08:24 -0600

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,48 @@
+// Function to create and display the menu overlay with fade-in animation
+function showMenuOverlay() {
+    const chatDisplay = document.getElementById('chat-display');
+    const menuOverlay = document.createElement('div');
+    menuOverlay.id = 'menu-overlay';
+    menuOverlay.innerHTML = `
+        <div id="menu-content">
+            <h2>Menu</h2>
+            <ul>
+                <li class="menu-item">User Settings</li>
+                <ul>
+                    <li class="menu-item">Note Settings</li>
+                    <li class="menu-item">Event Settings</li>
+                    <li class="menu-item">Task Settings</li>
+                </ul>
+                <li class="menu-item">System Settings</li>
+                <ul>
+                    <li class="menu-item" id="clear-data">Clear Data</li>
+                </ul>
+            </ul>
+        </div>
+    `;
+    menuOverlay.classList.add('menu-overlay');
+    menuOverlay.onclick = hideMenuOverlay; // Hide overlay on click anywhere on the overlay
+    menuOverlay.querySelector('#menu-content').onclick = function(event) {
+        event.stopPropagation(); // Prevents the overlay from closing when clicking on menu-content
+    };
+    document.getElementById('clear-data').onclick = function() {
+        if (window.confirm('Are you sure you want to clear all data?')) {
+            localStorage.clear();
+            location.reload();
+        }
+    };
+    chatDisplay.appendChild(menuOverlay);
+    setTimeout(() => { menuOverlay.style.opacity = '1'; }, 10); // Trigger fade-in
+}
+
+// Function to remove the menu overlay with fade-out animation
+function hideMenuOverlay() {
+    const menuOverlay = document.getElementById('menu-overlay');
+    if (menuOverlay) {
+        menuOverlay.style.opacity = '0';
+        setTimeout(() => { menuOverlay.remove(); }, 300); // Wait for fade-out to complete
+    }
+}
+
+// Event listener to show the menu on Menu button click
+document.getElementById('menu-button').addEventListener('click', showMenuOverlay);
